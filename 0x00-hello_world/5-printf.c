47d2998b8641cffca874e391603ca80e41621808
George Esetevbe <georgeperfect4u@gmail.com>
2022-09-09 01:36:17 +0100

    main tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* this is example of how strings
+ * are printedwith Printf.
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return(0);
+}
