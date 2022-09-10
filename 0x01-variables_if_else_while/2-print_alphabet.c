4c79940f7474400b25dcd642bb63a9c2c22a770e
George Esetevbe <georgeperfect4u@gmail.com>
2022-09-10 07:02:16 +0100

    2-aprint alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Return: always zero
+*/
+int main(void)
+{
+	unsigned char n = 0;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'A'; n <= 'Z'; n++)
+	{
+		putchar(n);
+	}
+	putchar(010);
+	return (0);
+}
