31ccb4c9d14a285fca81c221d9d492b2242c729c
George Esetevbe <georgeperfect4u@gmail.com>
2022-09-11 22:34:31 +0100

    wc was 34649 on the dot

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,26 +10,31 @@
 int main(void)
 {
 	unsigned int fst = 0;
-	unsigned int sec = 1;
+	unsigned int sec = 0;
 
 	while (fst < 99)
 	{
 		while (sec < 100)
 		{
+			if(fst == 0 && sec == 0)
+			{
+				sec++;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
 			putchar((fst / 10) + 48);
 			putchar((fst % 10) + 48);
 			putchar(' ');
 			putchar((sec / 10) + 48);
 			putchar((sec % 10) + 48);
 			sec++;
-			if (sec != 99 && fst != 98)
-			{
-				putchar(',');
-				putchar(' ');
-			}
 		}
 		fst++;
 		sec = fst + 1;
 	}
+	putchar(10);
 	return (0);
 }
