0f7563c422ae75b1f0ecbbd170bd84b5a1edc9b2
George Esetevbe <georgeperfect4u@gmail.com>
2022-09-16 00:25:14 +0100

    the numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers.
+ * @a: first number.
+ * @b: second number.
+ * Return: multiplied value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+deep dive into nested loops and functions
