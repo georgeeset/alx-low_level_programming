f1bf87b04023f0942106de8fa5b24adf85fc2ff0
George Esetevbe <georgeperfect4u@gmail.com>
2022-09-21 20:41:16 +0100

    check code now active

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - output concatinated string
+ *
+ * @dest: holds concatinated string
+ * @src: holds one of the source string
+ * @n: number of char to be added to dest from src
+ * Return: concanited string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int srclen = 0;
+	int destlen = 0;
+
+	do
+		destlen++;
+	while (dest[destlen] != '\0');
+
+	do {
+		*(dest + destlen + srclen) = src[srclen];
+		srclen++;
+	} while ((src[srclen] != '\0') && (srclen < n));
+
+	return (dest);
+}
