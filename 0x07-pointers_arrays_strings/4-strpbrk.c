3f86d520e06188209a576dc2c7331c480ded5511
George Esetevbe <georgeperfect4u@gmail.com>
2022-09-27 16:54:31 +0100

    repair 1

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -16,12 +16,12 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; *(accept + i) != '\0'; i++)
 	{
-		for (j = 0; *(accept + j) != '\0'; j++)
+		for (j = 0; *(s + j) != '\0'; j++)
 		{
-			if (*(s + i) == *(accept + j))
-				return (s + i);
+			if (*(s + j) == *(accept + i))
+				return (s + j);
 		}
 	}
 	return ('\0');
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -37,5 +37,5 @@ char *_strstr(char *haystack, char *needle)
 	}
 	if (found)
 		return (haystack + start);
-	return (NULL);
+	return ('\0');
 }
