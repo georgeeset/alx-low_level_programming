f16a6fc040c2ce5a8f09096b9f2e9d9a33d0b5d0
George Esetevbe <georgeperfect4u@gmail.com>
2022-09-27 04:36:12 +0100

    strspn focus

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,14 +14,19 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0;
-	unsigned int j;
+	unsigned int i, j;
 
-	for (j = 0; *(s + j) != '\0'; j++)
+	for (i = 0; *(accept + i) != '\0'; i++)
 	{
-		if (*(s + j) == *(accept + i))
-			return (j + 1);
+		for (j = 0; *(s + j) != '\0'; j++)
+		{
+			if (*(s + j) == *(accept + i))
+			{
+				return (j + 1);
+			}
+		}
 	}
-
+	if (*(accept + i) == *(s + j))
+		return (j);
 	return (0);
 }
