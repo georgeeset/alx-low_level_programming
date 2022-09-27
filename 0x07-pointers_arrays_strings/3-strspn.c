ef00f38614e27b4ec9e37bb160141acf662af1fb
George Esetevbe <georgeperfect4u@gmail.com>
2022-09-27 07:01:58 +0100

    community support

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,17 +14,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	unsigned int i, j, bool;
 
-	for (i = 0; *(accept + i) != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; *(s + j) != '\0'; j++)
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (*(s + j) == *(accept + i))
+			if (*(s + i) == *(accept + j))
 			{
-				return (j + 1);
+				bool = 0;
+				break;
 			}
 		}
+		if (bool == 1)
+			break;
 	}
-	return (j);
+	return (i);
 }
