ff44dd36f48ad5f0b441faf58d1a76075cce7ae9
George <georgeperfect4u@gmail.com>
2022-09-29 07:36:59 -0700

    merge corrected

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,10 +6,7 @@ void _puts_recursion(char *s);
 void _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
-<<<<<<< HEAD
 int _pow_recursion(int x, int y);
-=======
 int factorial(int n);
->>>>>>> 3c2ca82e0e34a610b63f6783995a6f1a7baf63a7
 
 #endif
