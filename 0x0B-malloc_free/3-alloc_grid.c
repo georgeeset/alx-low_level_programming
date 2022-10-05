00157ecad4049719b2b099574ed2b8f9e6c79e15
George <georgeperfect4u@gmail.com>
2022-10-05 00:45:21 -0700

    if you dream of beating me, you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * alloc_grid - generate dynamic two dimenaional array
+ * @width: lwngth of each array
+ * @height: number of arrays in list
+ * Return: null if:
+ * operation fails,
+ * either height or width is zero
+ * Else. address to array will be returned
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **mem;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	mem = (int **) malloc(height * sizeof(int *));
+	if (mem == NULL)
+	{
+		free(mem);
+	}
+	for (i = 0; i < height; i++)
+	{
+		*(mem + i) = (int *) malloc(width * sizeof(int));
+		if (*(mem + i) == NULL)
+		{
+			free(mem);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			mem[i][j] = 0;
+		}
+	}
+	return (mem);
+}
