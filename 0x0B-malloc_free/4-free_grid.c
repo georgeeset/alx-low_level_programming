9e1f7dd33681138ee453b36d5b1b79f0a40b77ef
George Esetevbe <georgeperfect4u@gmail.com>
2022-10-05 23:57:20 +0100

    memory leak essue

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -9,10 +9,8 @@
 
 void free_grid(int **grid, int height)
 {
-	int i;
-
 	if (grid == NULL || height <= 0)
 		return;
-	for (i = 0; i <= height; i++)
-		free(*(grid + i));
+	for (; height >= 0; height--)
+		free(*(grid + height));
 }
