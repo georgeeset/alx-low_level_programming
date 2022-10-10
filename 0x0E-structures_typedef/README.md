3180e4fcd225768585a37828187a2c4918a6a86b
George Esetevbe <georgeperfect4u@gmail.com>
2022-10-10 23:06:26 +0100

    A dog is theonly thing on earth you love more than yourself

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+##C - Structures, typedef##
+*take a deep dive in structures*
+*how to define structures*
+*how to use Structures*
+*how to make coding better with typedef*
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - struct that stores properties of a dog
+ * @name: name proterty of dog
+ * @age: age of dog
+ * @owner: name of dog owner
+ * Description: this structure called dog stores info
+ * about dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+void init_dog(struct dog *d,char *name,float age, char *owner);
+
+#endif
