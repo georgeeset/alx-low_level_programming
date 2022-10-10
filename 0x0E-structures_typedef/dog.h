1cf62e72d4b24ed0e9c97ba6cec04e857ffe185d
George Esetevbe <georgeperfect4u@gmail.com>
2022-10-10 23:08:33 +0100

    dog.h betty error cleared

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,6 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog;
-void init_dog(struct dog *d,char *name,float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
