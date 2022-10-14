93efa55734fd17c1ccfbf8f053f16420fe01bfa5
George Esetevbe <georgeperfect4u@gmail.com>
2022-10-14 03:43:46 +0100

    code refactored to for loop

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,27 +2,23 @@
 
 /**
  * print_numbers - prints numbers followed by a new line
- * @seperator: character to be printed between numbers
+ * @separator: character to be printed between numbers
  * @n: number of intigers to print
  * Return: void
  */
 
-void print_numbers(const char *seperator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
 	va_list num;
 
 	va_start(num, n);
-	if ((seperator != NULL) && (n > 0))
+	for (; i < n; i++)
 	{
-		while (i < (n - 1))
-		{
-			printf("%d%s", va_arg(num, int), seperator);
-			i++;
-		}
 		printf("%d", va_arg(num, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
 	}
+	putchar(10);
 	va_end(num);
-	if (n != 0)
-		putchar(10);
 }
