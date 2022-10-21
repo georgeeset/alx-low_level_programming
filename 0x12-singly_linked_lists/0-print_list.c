3a6bfb66edbace76f386a02fb296c4b2d457113e
George Esetevbe <georgeperfect4u@gmail.com>
2022-10-22 00:21:39 +0100

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the lements of a list_t list
+ * @h: pointer to the first list_t in the list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	list_t *point;
+	unsigned int i = 0;
+
+	point =(list_t *) h;
+	while (point != NULL)
+	{
+		if (point->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", point->len, point->str);
+
+		point = point->next;
+		i++;
+	}
+	return (i);
+}
