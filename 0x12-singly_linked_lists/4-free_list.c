95c5cb7e511d32624c7c765d16e0311b37d501f8
George Esetevbe <georgeperfect4u@gmail.com>
2022-10-22 03:18:27 +0100

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head:pointing to first node
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *h;
+
+	h = head;
+	if (head == NULL)
+		return;
+	while (h)
+	{
+		h = h->next;
+		free(head);
+		head = h;
+	}
+}
