518f73aec519e4dc30a3ee756e7b7c06770073eb
George Esetevbe <georgeperfect4u@gmail.com>
2022-10-24 23:12:07 +0100

    requirements fufilled

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements
+ * listint_len - count and returns the number of elements
  * in a linked listint_t list
  * @h: pointer to first node
+ * Return: number of elements
  */
 
 size_t listint_len(const listint_t *h)
@@ -11,7 +12,7 @@ size_t listint_len(const listint_t *h)
 	listint_t *pnt;
 	size_t cnt = 0;
 
-	if (! h)
+	if (!h)
 		return (0);
 
 	pnt = (listint_t *) h;
