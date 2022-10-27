9c51c2503d73a05919d129eb06176df98b2d069e
George Esetevbe <georgeperfect4u@gmail.com>
2022-10-27 03:30:41 +0100

    sun list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns sum of all the data (n)
+ * of a list_t linked list
+ * @head: first node of linked linst
+ *Return: sum of all data or 0 if list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr;
+	int sum = 0;
+
+	if (!head)
+		return (sum);
+
+	ptr = head;
+	while (ptr)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;		
+	}
+	return (sum);
+}
