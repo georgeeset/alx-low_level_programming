51461820c8bd0b6cfd3a622d31565cca0ee89e39
George Esetevbe <georgeperfect4u@gmail.com>
2022-10-29 23:11:14 +0100

    The hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * first_function - prints a string before main function is executed
+ * Return: void
+ */
+void first_function(void) __attribute__((constructor));
+
+void first_function(void)
+{
+	printf("You're breat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
