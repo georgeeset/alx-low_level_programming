718ec61c0075ef71a5ffcfe05519dd1769ee1fdf
George <georgeperfect4u@gmail.com>
2022-10-30 02:19:20 -0700

    How many lets does a dog have if you call his tail leg? Four. saying that a tail is a leg doesn't make it a leg

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dugs
+ * @d: pointer to created dog
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,6 @@
 #ifndef _DOG_H
 #define _DOG_H
-
+#include <stdlib.h>
 /**
  * struct dog - struct that stores properties of a dog
  * @name: name proterty of dog
@@ -20,5 +20,6 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
