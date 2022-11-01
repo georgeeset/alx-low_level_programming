38e59e66cb4599cba8abd2fd6a5cc8f1280d4487
George Esetevbe <georgeperfect4u@gmail.com>
2022-11-01 22:37:31 +0100

    Tread lightly

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * Reads a text and prints it to posix standart output
+ * read_textfile - Reads a text and prints it to posix standart output
  * @filename: name of file to read
- * @size: length of string to pa
+ * @letters: length of string to read
+ *
  * Return: number of letters it could read and print
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fl = 0;
@@ -15,13 +17,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename || !letters)
 		return (0);
 
-	str = malloc(letters * sizeof(char));
-	if(str == NULL)
-		return (0);
-
 	fl = open(filename, O_RDONLY);
 	if (fl == -1)
 		return (0);
+
+	str = malloc(letters * sizeof(char));
+	if (str == NULL)
+	{
+		close(fl);
+		return (0);
+	}
 
 	length = read(fl, str, letters);
 	if (length == -1)
