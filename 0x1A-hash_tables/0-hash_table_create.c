4d1f82a07a0bc515f61dc8b33409f7b335e38f73
georgeeset <georgeperfect4u@gmail.com>
2023-01-06 03:03:38 +0100

    create a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: is the size of the array
+ * Return: pointer to the newly created array or NULL
+ * if any thing goes wrong
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	hash_node_t **array;
+
+	table = malloc(sizeof(hash_table_t));
+	if(table == NULL)
+		return (NULL);
+
+	array = calloc(size, sizeof(hash_node_t *));
+	if(array == NULL)
+		return (NULL);
+
+	table->array = array;
+	table->size = size;
+	return (table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,4 @@
+### C - Hash Tables
+____________________
+** We demonstate the use of hash tables and hash maps to manage data,
+store data, Query data efficiently
