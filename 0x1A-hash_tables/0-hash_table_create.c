fdcdacf5fbc6824bcb18f7869064d9abdf8e6d6b
georgeeset <georgeperfect4u@gmail.com>
2023-01-06 03:05:30 +0100

    betty compliance

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,11 +13,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **array;
 
 	table = malloc(sizeof(hash_table_t));
-	if(table == NULL)
+	if (table == NULL)
 		return (NULL);
 
 	array = calloc(size, sizeof(hash_node_t *));
-	if(array == NULL)
+	if (array == NULL)
 		return (NULL);
 
 	table->array = array;
