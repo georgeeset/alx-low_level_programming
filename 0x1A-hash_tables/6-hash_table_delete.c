29c3574cd31a861a7ec99c63931be2e1d186405a
georgeeset <georgeperfect4u@gmail.com>
2023-01-14 05:56:16 +0100

    delete hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,46 @@
+#include "hash_tables.h"
+
+/**
+ * free_linked_list - free linkedlist =
+ * @head: pointer to linked list head
+ */
+void free_linked_list(hash_node_t *head)
+{
+	hash_node_t *h;
+
+	h = head;
+	if (!head)
+		return;
+	while ((h = head) != NULL)
+	{
+		head = head->next;
+		free(h->key);
+		free(h->value);
+		free(h);
+	}
+}
+
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: is the hash table
+ * Return: NULL
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t **node_list;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+	node_list = ht->array;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (node_list[i])
+		{
+			free_linked_list(node_list[i]);
+		}
+	}
+	free(ht);
+}
